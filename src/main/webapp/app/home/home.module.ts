22c816dc9db86a036f1ffb8f9290a494359e7ace
akatech4 <aboucissoko.sps@gmail.com>
2021-05-11 23:33:57 +0000

    Update Home

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), MatDatepickerModule],
   declarations: [HomeComponent],
+  exports: [MatDatepickerModule],
 })
 export class HomeModule {}
